bd182df1e73cc5ad910d512e1d1ec904efaf2c28
Daniel Ramos <danielramos@Daniels-MacBook-Pro-2.local>
2018-02-21 22:18:54 -0500

    main app for comp2106

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET About page. */
+router.get('/about', (req, res, next) => {
+  res.render('about', {
+    title: 'About Car Tracker',
+    message: 'This app is built with MEAN stack.'
+  });
+});
+
+/* GET About page. */
+router.get('/contact', (req, res, next) => {
+  res.render('contact', {
+    title: 'Contact Us',
+    message: 'You are now at the contact page.'
+  });
+});
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { 
+    title: 'Car Tracker',
+    message: 'COMP2106 In-Class Node Application' 
+  });
+});
+
+module.exports = router;
