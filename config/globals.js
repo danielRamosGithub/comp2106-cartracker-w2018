5264b83789c3467188a8e5b60a6d57c8adcf58ee
Daniel Ramos <danielramos@Daniels-MacBook-Pro-2.local>
2018-03-11 15:11:57 -0400

    add and edit functionality update to use MLAB for database.

--- config/globals.js
+++ config/globals.js
@@ -1,4 +1,5 @@
 
 module.exports = {
-    'db' : 'mongodb://localhost:27017/cartracker'
+    // 'db' : 'mongodb://localhost:27017/cartracker'
+    'db' : 'mongodb://danieldba:danieldba123@ds012538.mlab.com:12538/cartracker'
 }
--- routes/cars.js
+++ routes/cars.js
@@ -41,5 +41,71 @@ router.post('/add', (req, res, next) => {
     });
 });
 
+// GET: /cars/delete/abc123
+router.get('/delete/:_id', (req, res, next) => {
+    // get the _id parameter from the url and store in a local variable
+    let _id = req.params._id;
+
+    // use the Car model to delete the document with this id
+    Car.remove({_id: _id}, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/cars');
+        }
+    });
+});
+
+// GET: /cars/edit/abc123
+router.get('/edit/:_id', (req, res, next) => {
+    let _id = req.params._id;
+
+    // use Car model to find the selected document
+    Car.findById(_id, (err, car) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('cars/edit', {
+               title: 'Car Details',
+               car: car 
+            });
+        }
+    });
+});
+
+// POST: /cars/edit/abc123
+router.post('/edit/:_id', (req, res, next) => {
+    // get the _id from URL
+    let _id = req.params._id;
+
+    // populate a car object from the from post values
+    // let car = new Car({
+    //     make: req.body.make,
+    //     model: req.body.model,
+    //     year: req.body.year,
+    //     colour: req.body.colour
+    // });
+
+    // call Mongoose update method, passing the _id and the new car object
+    Car.update({_id: _id}, 
+        {
+            $set: {
+                make: req.body.make,
+                model: req.body.model,
+                year: req.body.year,
+                colour: req.body.colour
+            }
+        }, null, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect('/cars');
+            }
+    });
+
+
+
+});
+
 // make public
 module.exports = router;
